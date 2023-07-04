6b3d12191f4d79e1557e03d9abe25127c750f64f
Igor Parfenov <lloopy123123543@gmail.com>
2023-07-04 15:05:50 +0500

    sus

--- front/app/components/hueder.tsx
+++ front/app/components/hueder.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function HUEDER(props : any) {
-    console.log(props)
+
   return (
     <div>{props.name == "" ? <div>Нету</div> : <div>Есть</div>}</div>
     )
--- front/app/text/page.tsx
+++ front/app/text/page.tsx
@@ -1,17 +1,50 @@
 "use client"
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import HUEDER from '../components/hueder'
-
+import Cars from './cars'
+import CAR from './car'
 
 export default function TEST() {
+  const [Cures, setCures] = useState([{}])
+  const [name, setname] = useState('')
+  const [cost, setcost] = useState('')
 
-    
+  useEffect(() => {
+    setCures(CAR)
+
+  }, [])
+
+  const checkName = (e:any) => {
+    setname(e.target.value)
+  }
+
+  const checkCost = (e:any) => {
+    setcost(e.target.value)
+  }
+
+  const sendCar = (e:any) => {
+    e.preventDefault();
+
+    const newCar = {
+      id: Cures.length + 1,
+      name: name,
+      cost: cost
+    }
+
+    setCures(prev => [...prev, newCar])
+  }
+
   return (
     <div>
-        <HUEDER name = "ы"></HUEDER>
+      <HUEDER name="ы" />
+      <div>
+        <form>
+          <input placeholder='name' onChange={checkName}></input>
+          <input placeholder='cost' onChange={checkCost}></input>
+          <button onClick={sendCar}>privet</button>
+ </form>
+        <Cars car={Cures} />
+      </div>
     </div>
-    // <div>{Folder.map(sus => {
-    //     return( <div key={sus.id}>sus</div>)
-    // })}</div>
   )
 }
