130c987055290ee4595a8adaf517e29e50754d70
Igor Parfenov <lloopy123123543@gmail.com>
2023-07-07 14:58:47 +0500

    animations

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,6 +1,8 @@
+"use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
 import HEADER from './components/header'
+import { motion, AnimatePresence } from "framer-motion"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +21,15 @@ export default function RootLayout({
       <body className={inter.className}>
         
         <HEADER></HEADER>
-        {children}
+        <AnimatePresence>
+          <motion.div
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+              >
+          {children}
+          </motion.div>
+        </AnimatePresence>
         </body>
     </html>
   )
